c968201b00f960db80d86d524217da5a153d080b
Patrik <patr5009@elev.techcollege.dk>
2023-02-12 21:17:23 +0100

    Contact WIP

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,59 @@
 import { Galaxy } from "../comp/galaxy/Galaxy.jsx";
 import { MetaTags } from "../comp/MetaTags.jsx";
-
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export const Contact = (props) => {
+  const form = useRef();
 
-    return (
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        <section id="Contact">
+    emailjs
+      .sendForm(
+        "service_zltc55m",
+        "template_qw53jhd",
+        form.current,
+        "PWAaqSbEwQXEo4NqD"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <section id="Contact">
       <MetaTags title={props.title}></MetaTags>
 
+      <Galaxy />
       <main>
-        <Galaxy/>
+        <h1>kontakt</h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <div>
+            <div>
+              <input type="text" name="company_name" placeholder="Firma navn" />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="user_name"
+                placeholder="navn"
+              />
+            </div>
+          </div>
+          <div>
+            <input className="Mail" type="email" name="user_email" placeholder="Email" />
+          </div>
+          <div>
+            <textarea name="message" placeholder="skriv Besked her" />
+          </div>
+          <input type="submit" value="Send" />
+        </form>
       </main>
-
-
-
-        </section>
-    )
-
-
-}
+    </section>
+  );
+};
